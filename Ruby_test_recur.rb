9b33e0dc0ee8a4a7903b77de15ea49285a134101
scalar0816 <scalar0816@naver.com>
2026-08-13 15:36:39 +0900

    20260813153600 RubyRuby

--- Ruby_test_recur.rb
+++ Ruby_test_recur.rb
@@ -18,11 +18,13 @@ def add_num(*args)
     total
 end
 
-# 아래와 같이 적으면 더 편해
+p add_num(*[1,2,3,4,5])
+
+# 위에 건 아래와 같이 적으면 더 편해
 # TODO: 일단은 음수가 들어왔을 땐 생각 안했어
 # def recur(base, exponent)
 #   exponent == 0 ? 1 : base * recur(base, exponent - 1)
 # end
 
-
-p add_num(*[1,2,3,4,5])
+# 두 번째 건 그냥 sum 해버려도 되지만
+# 지금 단계에선 직접 구현해 보는 것도 나쁘진 않아.
