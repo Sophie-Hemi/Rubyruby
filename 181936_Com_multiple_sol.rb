b4a61fce07b16eab05374b4045ff7d5125eaa0d1
scalar0816 <scalar0816@naver.com>
2026-08-14 12:01:22 +0900

    202608141200 Rubyruby

--- 181936_Com_multiple_sol.rb
+++ 181936_Com_multiple_sol.rb
@@ -2,5 +2,8 @@ def solution(number, n, m)
     ((number%n==0)&(number%m==0))? answer = 1: answer = 0
 end
 
-p solution(60,2,3)
-p solution(55,11,5)
+p solution(60,2,3) #1
+p solution(55,10,5) #0
+
+# ?는 "if true"의 역할을,
+# :는 "else"의 역할을 하고 있음.
--- Ruby_test_recur.rb
+++ Ruby_test_recur.rb
@@ -20,11 +20,18 @@ end
 
 p add_num(*[1,2,3,4,5])
 
+# Ruby에서는 return [출력값]하고 써도 되지만,
+# 굳이 그럴 필요 없이 알아서 return된다.
+
+# 조건문은 "조건? 맞으면 이거: 아니면 이거" 같은 식으로 쓰기도 하고,
+# if, elsif, else로 써 줘도 ok.
+# 그 대신 end의 위치를 잘 확인해야.
+
 # 위에 건 아래와 같이 적으면 더 편해
 # TODO: 일단은 음수가 들어왔을 땐 생각 안했어
 # def recur(base, exponent)
 #   exponent == 0 ? 1 : base * recur(base, exponent - 1)
 # end
 
-# 두 번째 건 그냥 sum 해버려도 되지만
+# 두 번째 건 그냥 args.sum 해버려도 되지만
 # 지금 단계에선 직접 구현해 보는 것도 나쁘진 않아.
